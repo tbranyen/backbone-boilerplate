d1e6b02e971c67919b3b407667c3e1f03551d518
tbranyen <tim@tabdeveloper.com>
2012-01-09 23:45:32 -0500

    moved tasks to separate folders and created auto-loader Closes GH-19

--- build/index.js
+++ build/index.js
@@ -1,6 +1,16 @@
+var fs = require("fs");
 var grunt = require("grunt");
+
+// Auto-load tasks
+var taskList = fs.readdirSync(__dirname + "/tasks").filter(function(task) {
+  return fs.statSync(__dirname + "/tasks/" + task).isDirectory();
+}).map(function(task) {
+  return "build/tasks/" + task;
+}).concat("build/tasks");
+
+console.log(taskList);
 
 grunt.cli({
   config: "build/config.js",
-  tasks: ["build/tasks"]
+  tasks: taskList
 });
