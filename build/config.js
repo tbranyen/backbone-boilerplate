162562b9a29f2e78bce958df784831e3e3d63b7d
unknown <Kurt Milam@.(none)>
2012-01-24 00:03:57 +0100

    plit namespace declaration from app entry point
    See previous commit message for more details.

--- build/config.js
+++ build/config.js
@@ -18,7 +18,7 @@ config.init({
     ],
 
     // Application files
-    "dist/debug/js/app.js": ["app/*.js", "app/modules/*.js"],
+    "dist/debug/js/app.js": ["app/index.js", "app/modules/*.js", "app/app.js"],
 
     // Your CSS
     "dist/debug/css/style.css": ["assets/css/*.css"]
