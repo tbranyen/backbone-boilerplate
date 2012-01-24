3678897e8ab0a051b64264832c8da5e8a6c79d51
kurtmilam <kurt.milam@xioup.com>
2012-01-24 17:14:14 +0100

    Updated app, css and js paths in build/config.js
    Changed source paths from, for example:
    ["app/templates/*.html"]
    to:
    ["app/templates/**/*.html"]
    in order to automatically find files in subdirectories (may be useful for
    organization in larger) projects.

--- build/config.js
+++ build/config.js
@@ -18,14 +18,14 @@ config.init({
     ],
 
     // Application files
-    "dist/debug/js/app.js": ["app/namespace.js", "app/modules/*.js", "app/index.js"],
+    "dist/debug/js/app.js": ["app/namespace.js", "app/modules/**/*.js", "app/index.js"],
 
     // Your CSS
-    "dist/debug/css/style.css": ["assets/css/*.css"]
+    "dist/debug/css/style.css": ["assets/css/**/*.css"]
   },
-  
+
   jst: {
-    "dist/debug/js/templates.js": ["app/templates/*.html"]
+    "dist/debug/js/templates.js": ["app/templates/**/*.html"]
   },
 
   min: {
