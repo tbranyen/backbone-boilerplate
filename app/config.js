d49a328cbcdbaff4db04800db9e432259ae569bf
tim <tim@tabdeveloper.com>
2012-02-26 23:34:49 -0500

    added config.js

---
+++ app/config.js
@@ -0,0 +1,30 @@
+// Set the require.js configuration for your application.
+require.config({
+  // Initialize the application with the main application file
+  deps: ["main"],
+
+  paths: {
+    // JavaScript folders
+    libs: "../assets/js/libs",
+    plugins: "../assets/js/plugins",
+
+    // Libraries
+    jquery: "../assets/js/libs/jquery",
+    underscore: "../assets/js/libs/underscore",
+    backbone: "../assets/js/libs/backbone",
+
+    // Shim Plugin
+    use: "../assets/js/plugins/use"
+  },
+
+  use: {
+    backbone: {
+      deps: ["use!underscore", "jquery"],
+      attach: "Backbone"
+    },
+
+    underscore: {
+      attach: "_"
+    }
+  }
+});
