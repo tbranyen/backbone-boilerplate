029c435df1df10fc0e02cbc089880e9a5920f08d
tbranyen <tim@tabdeveloper.com>
2012-06-19 17:59:13 -0400

    reworked router out into separate file

--- app/config.js
+++ app/config.js
@@ -1,23 +1,26 @@
 // Set the require.js configuration for your application.
 require.config({
-  // Initialize the application with the main application file
+
+  // Initialize the application with the main application file.
   deps: ["main"],
 
   paths: {
-    // JavaScript folders
+    // JavaScript folders.
     libs: "../assets/js/libs",
     plugins: "../assets/js/plugins",
 
-    // Libraries
+    // Libraries.
     jquery: "../assets/js/libs/jquery",
     lodash: "../assets/js/libs/lodash",
     backbone: "../assets/js/libs/backbone"
   },
 
   shim: {
+    // Backbone library depends on lodash and jQuery.
     backbone: {
       deps: ["lodash", "jquery"],
       exports: "Backbone"
     }
   }
+
 });
