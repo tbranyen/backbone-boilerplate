e4904b2f30c5b0dd5dd95da429f8471a35cf1ea2
tbranyen <tim@tabdeveloper.com>
2012-07-08 20:27:21 -0400

    removed default command

--- grunt.js
+++ grunt.js
@@ -146,16 +146,12 @@ module.exports = function(grunt) {
 
   });
 
-  // The default task will remove all contents inside the dist/ folder, lint
+  // The debug task will remove all contents inside the dist/ folder, lint
   // all your code, precompile all the underscore templates into
   // dist/debug/templates.js, compile all the application code into
   // dist/debug/require.js, and then concatenate the require/define shim
   // almond.js and dist/debug/templates.js into the require.js file.
-  grunt.registerTask("default", "clean lint jst requirejs concat");
-
-  // The debug task is simply an alias to default to remain consistent with
-  // debug/release.
-  grunt.registerTask("debug", "default");
+  grunt.registerTask("debug", "clean lint jst requirejs concat");
 
   // The release task will run the debug tasks and then minify the
   // dist/debug/require.js file and CSS files.
