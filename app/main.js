6a04960311e560559cf1621a71a14dc0feb831fd
tbranyen <tim@tabdeveloper.com>
2012-08-03 20:32:33 -0400

    fixed anchor addressing

--- app/main.js
+++ app/main.js
@@ -21,12 +21,12 @@ function(app, Router) {
   // attribute, bypass the delegation completely.
   $(document).on("click", "a:not([data-bypass])", function(evt) {
     // Get the absolute anchor href.
-    var href = $(this).prop("href");
+    var href = { prop: $(this).prop("href"), attr: $(this).attr("href") };
     // Get the absolute root.
     var root = location.protocol + "//" + location.host + app.root;
 
     // Ensure the root is part of the anchor href, meaning it's relative.
-    if (href && href.slice(0, root.length) === root) {
+    if (href.prop && href.prop.slice(0, root.length) === root) {
       // Stop the default event to ensure the link will not cause a page
       // refresh.
       evt.preventDefault();
@@ -34,7 +34,7 @@ function(app, Router) {
       // `Backbone.history.navigate` is sufficient for all Routers and will
       // trigger the correct events. The Router's internal `navigate` method
       // calls this anyways.  The fragment is sliced from the root.
-      Backbone.history.navigate(href.slice(root.length), true);
+      Backbone.history.navigate(href.attr, true);
     }
   });
 
