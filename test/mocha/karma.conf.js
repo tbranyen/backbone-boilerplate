83070c124cbf11d8eaa7aa95337a14e0dff53e25
Trevor Cook <trevdc@gmail.com>
2013-08-04 18:24:18 -0700

    Fix path to chai in mocha karma.conf.js

--- test/mocha/karma.conf.js
+++ test/mocha/karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     frameworks: [ "mocha" ],
     files: [
-      "test/mocha/vendor/chai.js",
+      "vendor/bower/chai/chai.js",
       "vendor/bower/requirejs/require.js",
       "test/mocha/test-runner.js"
     ]
