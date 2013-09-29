a31f35910018544771cbcbc244e8688e9f98a43e
Tim Branyen <tim@tabdeveloper.com>
2013-09-29 13:06:19 -0400

    Explain the map operation in the runner.

--- test/runner.js
+++ test/runner.js
@@ -32,6 +32,7 @@
 
   function(config, _) {
     var specs = _.chain(karma.files)
+      // Convert the files object to an array of file paths.
       .map(function(id, file) { return file; })
       // Tests that end with `.spec.js' and existing either `app` or `test`
       // directories are automatically loaded.
