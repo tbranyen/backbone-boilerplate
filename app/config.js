36cec82a2e5860fbf68e73be44b7392866984982
Tim Branyen <tim@tabdeveloper.com>
2014-02-13 15:23:17 -0500

    Upgraded dependencies, removed unnecssary shim configuration.  Thanks @jashkenas!

--- app/config.js
+++ app/config.js
@@ -2,29 +2,10 @@
 // supplementing shared properties.
 require.config({
   paths: {
-    // Make vendor easier to access.
     "vendor": "../vendor",
-
-    // Almond is used to lighten the output filesize.
     "almond": "../vendor/bower/almond/almond",
-
-    // Opt for Lo-Dash Underscore compatibility build over Underscore.
     "underscore": "../vendor/bower/lodash/dist/lodash.underscore",
-
-    // Map remaining vendor dependencies.
     "jquery": "../vendor/bower/jquery/jquery",
     "backbone": "../vendor/bower/backbone/backbone"
-  },
-
-  shim: {
-    // This is required to ensure Backbone works as expected within the AMD
-    // environment.
-    "backbone": {
-      // These are the two hard dependencies that will be loaded first.
-      deps: ["jquery", "underscore"],
-
-      // This maps the global `Backbone` object to `require("backbone")`.
-      exports: "Backbone"
-    }
   }
 });
