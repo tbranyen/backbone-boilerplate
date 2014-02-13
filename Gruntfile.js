673c32fa07e5c69014964e9f81e0c3ef885b01a6
Tim Branyen <tim@tabdeveloper.com>
2014-02-13 15:39:16 -0500

    Hopefully fixes Coveralls errors.

--- Gruntfile.js
+++ Gruntfile.js
@@ -187,7 +187,7 @@ module.exports = function(grunt) {
 
     coveralls: {
       options: {
-        coverage_dir: "test/coverage/PhantomJS 1.9.2 (Linux)/"
+        coverage_dir: "test/coverage/"
       }
     }
   });
