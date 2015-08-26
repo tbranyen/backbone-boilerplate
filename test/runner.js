ed7ac1a2efb65a58b2d6940ca29f3b9672eb04ed
Tim Branyen <tim@tabdeveloper.com>
2015-08-26 00:21:48 -0400

    Remove unused code

--- test/runner.js
+++ test/runner.js
@@ -1,13 +1,2 @@
 import './tests/example';
 import './tests/router';
-
-var karma = window.__karma__;
-
-if (window.QUnit) {
-  // Disable auto start.  We'll call start once the async modules have
-  // loaded.
-  window.QUnit.config.autostart = false;
-} else if (window.chai) {
-  // Optionally use chai with Mocha.
-  window.expect = window.chai.expect;
-}
