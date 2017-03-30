aca191a8ce5f1bbf33d4674497796c1f4e5ffa2a
Petar Krivoshiev <petar@codeberry.io>
2017-03-30 08:36:03 +0300

    Revert "Converting tabs to spaces."
    This reverts commit aca5ea78363bed0e3a6b9d7993755022690a06a4.

--- test/tests/example.js
+++ test/tests/example.js
@@ -1,52 +1,46 @@
-describe("Simple tests examples", function () {
-
-  it("should fail", function () {
-    assert.equal(false, false);
-  });
-
-  it("should detect true", function () {
+describe("Simple tests examples", function() {
+  it("should detect true", function() {
     assert.equal(true, true);
   });
 
-  it("should increments values", function () {
-    let mike = 0;
+  it("should increments values", function() {
+    var mike = 0;
 
     assert.equal(mike++ === 0, true);
     assert.equal(mike === 1, true);
   });
 
-  it("should increments values (improved)", function () {
-    let mike = 0;
+  it("should increments values (improved)", function() {
+    var mike = 0;
 
     assert.equal(mike++, 0);
     assert.equal(mike, 1);
   });
-
 });
 
-describe("Tests with before/after hooks", function () {
-  let a = 0;
+describe("Tests with before/after hooks", function() {
+  var a = 0;
 
-  beforeEach(function () {
+  beforeEach(function() {
     a++;
   });
 
-  afterEach(function () {
+  afterEach(function() {
     a = 0
   });
 
-  it("should increment value", function () {
+  it("should increment value", function() {
     assert.equal(a, 1);
   });
 
-  it("should reset after each test", function () {
+  it("should reset after each test", function() {
     assert.equal(a, 1);
   });
 });
 
-describe("Async tests", function () {
-  it("should wait timer", function (done) {
-    setTimeout(function () {
+describe("Async tests", function() {
+  it("should wait timer", function(done) {
+    setTimeout(function() {
       assert.equal(true, true);
       done();
     }, 500);
